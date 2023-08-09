7e155b978b363eea7e5681e92d6f5d3a5c9955a3
Iskandar <eiscocyber@gmail.com>
2023-08-09 22:41:53 +0500

    Quiz Dtos created

---
+++ src/LexiLearn.Service/DTOs/Quizzes/QuizCreationDto.cs
@@ -0,0 +1,16 @@
+﻿using LexiLearn.Domain.Enums;
+
+namespace LexiLearn.Service.DTOs.Quizzes;
+
+public class QuizCreationDto
+{
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public int Deadline { get; set; }
+
+    public QuizLevel Level { get; set; }
+
+    public long CategoryId { get; set; }
+}
---
+++ src/LexiLearn.Service/DTOs/Quizzes/QuizUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using LexiLearn.Domain.Enums;
+
+namespace LexiLearn.Service.DTOs.Quizzes;
+
+public class QuizUpdateDto
+{
+    public long Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public int Deadline { get; set; }
+
+    public QuizLevel Level { get; set; }
+
+    public long CategoryId { get; set; }
+}
