03707ea782dd26a53fad125ccd2e023447af7466
Iskandar <eiscocyber@gmail.com>
2023-08-09 22:47:25 +0500

    Word Dtos added

---
+++ src/LexiLearn.Service/DTOs/Words/WordCreationDto.cs
@@ -0,0 +1,18 @@
+﻿using LexiLearn.Domain.Enums;
+
+namespace LexiLearn.Service.DTOs.Words;
+
+public class WordCreationDto
+{
+    public string Text { get; set; }
+
+    public string Translation { get; set; }
+
+    public string Definition { get; set; }
+
+    public string Pronunciation { get; set; }
+
+    public Language Language { get; set; }
+
+    public long CategoryId { get; set; }
+}
---
+++ src/LexiLearn.Service/DTOs/Words/WordResultDto.cs
@@ -0,0 +1,20 @@
+﻿using LexiLearn.Domain.Enums;
+
+namespace LexiLearn.Service.DTOs.Words;
+
+public class WordResultDto
+{
+    public long Id { get; set; }
+
+    public string Text { get; set; }
+
+    public string Translation { get; set; }
+
+    public string Definition { get; set; }
+
+    public string Pronunciation { get; set; }
+
+    public Language Language { get; set; }
+
+    public long CategoryId { get; set; }
+}
