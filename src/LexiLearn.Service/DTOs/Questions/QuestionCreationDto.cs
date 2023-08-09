adc56760d744b2b1ee70be16f9ef87b435b37dce
Iskandar <eiscocyber@gmail.com>
2023-08-09 22:49:56 +0500

    Question DTOs writed

---
+++ src/LexiLearn.Service/DTOs/Questions/QuestionCreationDto.cs
@@ -0,0 +1,10 @@
+﻿using LexiLearn.Domain.Enums;
+
+namespace LexiLearn.Service.DTOs.Questions;
+
+public class QuestionCreationDto
+{
+    public QuestionType Type { get; set; }
+
+    public long WordId { get; set; }
+}
---
+++ src/LexiLearn.Service/DTOs/Questions/QuestionResultDto.cs
@@ -0,0 +1,12 @@
+﻿using LexiLearn.Domain.Enums;
+
+namespace LexiLearn.Service.DTOs.Questions;
+
+public class QuestionResultDto
+{
+    public long Id { get; set; }
+
+    public QuestionType Type { get; set; }
+
+    public long WordId { get; set; }
+}
