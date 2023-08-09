575cfeaeb669bfaa1339bf16b3cc8546ee692f58
Iskandar <eiscocyber@gmail.com>
2023-08-10 01:54:14 +0500

    Question service and interface added

---
+++ src/LexiLearn.Service/Interfaces/IQuestionService.cs
@@ -0,0 +1,24 @@
+﻿using LexiLearn.Domain.Entities.Questions;
+using LexiLearn.Service.DTOs.Questions;
+using LexiLearn.Service.Helpers;
+
+namespace LexiLearn.Service.Interfaces;
+
+public interface IQuestionService
+{
+    Task<Response<QuestionResultDto>> CreateAsync(QuestionCreationDto dto);
+
+    Task<Response<QuestionResultDto>> UpdateAsync(QuestionUpdateDto dto);
+
+    Task PrintQuestionAsync(long id);
+
+    Task<Response<bool>> DeleteAsync(long id);
+
+    Task<Response<Question>> GetByIdAsync(long id);
+
+    Task<Response<IEnumerable<Question>>> GetAllAsync();
+
+    Task<Response<IEnumerable<Question>>> GetQuestionsByWordAsync(long wordId);
+
+    Task<Response<bool>> CheckAsync(long id, string answer);
+}
