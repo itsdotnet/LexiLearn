9bc1a1635b90d30ed30cfc505d8aab2edd86e57f
Iskandar <eiscocyber@gmail.com>
2023-08-10 16:28:59 +0500

    QuizHistory updated WordCategoryControl added

--- src/LexiLearn/Views/AdminView/QuizHistoryControl.cs
+++ src/LexiLearn/Views/AdminView/QuizHistoryControl.cs
@@ -1,12 +1,126 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LexiLearn.Domain.Entities.Quizzes;
+using LexiLearn.Service.DTOs.Quizzes.QuizHistory;
+using LexiLearn.Service.Helpers;
+using LexiLearn.Service.Interfaces;
 
-namespace LexiLearn.Views.AdminView
+namespace LexiLearn.Service.Services;
+
+public class QuizHistoryControl
 {
-    internal class QuizHistoryControl
+    private readonly IQuizHistoryService quizHistoryService;
+
+    public QuizHistoryControl()
     {
+        this.quizHistoryService = new QuizHistoryService();
+    }
+
+    public async Task StartQuizHistoryControl()
+    {
+        bool isRunning = true;
+
+        while (isRunning)
+        {
+            Console.WriteLine("Quiz History Control");
+            Console.WriteLine("\n1. Create Quiz History");
+            Console.WriteLine("2. Get Quiz History by User ID");
+            Console.WriteLine("3. Get Quiz History by Quiz ID");
+            Console.WriteLine("4. Delete Quiz History");
+            Console.WriteLine("5. Exit");
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    await CreateQuizHistory();
+                    break;
+                case "2":
+                    await GetQuizHistoryByUserId();
+                    break;
+                case "3":
+                    await GetQuizHistoryByQuizId();
+                    break;
+                case "4":
+                    await DeleteQuizHistory();
+                    break;
+                case "5":
+                    isRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+
+    private async Task CreateQuizHistory()
+    {
+        Console.Write("Enter User ID: ");
+        long userId = Convert.ToInt64(Console.ReadLine());
+
+        Console.Write("Enter Quiz ID: ");
+        long quizId = Convert.ToInt64(Console.ReadLine());
+
+        Console.Write("Enter score: ");
+        long score = Convert.ToInt64(Console.ReadLine());
+
+        var creationDto = new QuizHistoryCreationDto
+        {
+            UserId = userId,
+            QuizId = quizId,
+            Score = score
+        };
+
+        var response = await quizHistoryService.CreateAsync(creationDto);
+
+        Console.WriteLine(response.Message);
+    }
+
+    private async Task GetQuizHistoryByUserId()
+    {
+        Console.Write("Enter User ID: ");
+        long userId = Convert.ToInt64(Console.ReadLine());
+
+        var response = await quizHistoryService.GetQuizHistoryByUserIdAsync(userId);
+
+        PrintQuizHistory(response);
+    }
+
+    private async Task GetQuizHistoryByQuizId()
+    {
+        Console.Write("Enter Quiz ID: ");
+        long quizId = Convert.ToInt64(Console.ReadLine());
+
+        var response = await quizHistoryService.GetQuizHistoryByQuizIdAsync(quizId);
+
+        PrintQuizHistory(response);
+    }
+
+    private async Task DeleteQuizHistory()
+    {
+        Console.Write("Enter Quiz History ID: ");
+        long id = Convert.ToInt64(Console.ReadLine());
+
+        var response = await quizHistoryService.DeleteAsync(id);
+
+        Console.WriteLine(response.Message);
+    }
+
+    private void PrintQuizHistory(Response<IEnumerable<QuizHistory>> response)
+    {
+        if (response.StatusCode == 200)
+        {
+            foreach (var quizHistory in response.Data)
+            {
+                Console.WriteLine($"Quiz History ID: {quizHistory.Id}");
+                Console.WriteLine($"User ID: {quizHistory.UserId}");
+                Console.WriteLine($"Quiz ID: {quizHistory.QuizId}");
+                Console.WriteLine("------------------------------");
+            }
+        }
+        else
+        {
+            Console.WriteLine(response.Message);
+        }
     }
 }
---
+++ src/LexiLearn/Views/AdminView/WordCategoryControl.cs
@@ -0,0 +1,148 @@
+﻿using LexiLearn.Service.DTOs.Categories;
+using LexiLearn.Service.Interfaces;
+
+namespace LexiLearn.Service.Services;
+
+public class WordCategoryControl
+{
+    private readonly IWordCategoryService wordCategoryService;
+
+    public WordCategoryControl()
+    {
+        this.wordCategoryService = new WordCategoryService();
+    }
+
+    public async Task StartWordCategoryControl()
+    {
+        bool isRunning = true;
+
+        while (isRunning)
+        {
+            Console.WriteLine("Word Category Control");
+            Console.WriteLine("1. Create Word Category");
+            Console.WriteLine("2. Update Word Category");
+            Console.WriteLine("3. Delete Word Category");
+            Console.WriteLine("4. Get Word Category by ID");
+            Console.WriteLine("5. Get All Word Categories");
+            Console.WriteLine("6. Exit");
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    await CreateWordCategory();
+                    break;
+                case "2":
+                    await UpdateWordCategory();
+                    break;
+                case "3":
+                    await DeleteWordCategory();
+                    break;
+                case "4":
+                    await GetWordCategoryById();
+                    break;
+                case "5":
+                    await GetAllWordCategories();
+                    break;
+                case "6":
+                    isRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+
+    private async Task CreateWordCategory()
+    {
+        Console.Write("Enter Word Category Name: ");
+        string categoryName = Console.ReadLine();
+
+        Console.Write("Enter Word Category Description: ");
+        string categoryDescription = Console.ReadLine();
+
+        var creationDto = new CategoryCreationDto
+        {
+            Title = categoryName,
+            Description = categoryDescription
+        };
+
+        var response = await wordCategoryService.CreateAsync(creationDto);
+
+        Console.WriteLine(response.Message);
+    }
+
+    private async Task UpdateWordCategory()
+    {
+        Console.Write("Enter Word Category ID: ");
+        long categoryId = Convert.ToInt64(Console.ReadLine());
+
+        Console.Write("Enter New Word Category Name: ");
+        string newCategoryName = Console.ReadLine();
+
+        Console.Write("Enter Word Category Description: ");
+        string categoryDescription = Console.ReadLine();
+
+        var updateDto = new CategoryUpdateDto
+        {
+            Id = categoryId,
+            Title = newCategoryName,
+            Description = categoryDescription
+        };
+
+        var response = await wordCategoryService.UpdateAsync(updateDto);
+
+        Console.WriteLine(response.Message);
+    }
+
+    private async Task DeleteWordCategory()
+    {
+        Console.Write("Enter Word Category ID: ");
+        long categoryId = Convert.ToInt64(Console.ReadLine());
+
+        var response = await wordCategoryService.DeleteAsync(categoryId);
+
+        Console.WriteLine(response.Message);
+    }
+
+    private async Task GetWordCategoryById()
+    {
+        Console.Write("Enter Word Category ID: ");
+        long categoryId = Convert.ToInt64(Console.ReadLine());
+
+        var response = await wordCategoryService.GetByIdAsync(categoryId);
+
+        if (response.StatusCode == 200)
+        {
+            Console.WriteLine($"Word Category ID: {response.Data.Id}");
+            Console.WriteLine($"Word Category Name: {response.Data.Title}");
+            Console.WriteLine($"Word Category Description: {response.Data.Description}");
+        }
+        else
+        {
+            Console.WriteLine(response.Message);
+        }
+    }
+
+    private async Task GetAllWordCategories()
+    {
+        var response = await wordCategoryService.GetAllAsync();
+
+        if (response.StatusCode == 200)
+        {
+            foreach (var category in response.Data)
+            {
+                Console.WriteLine($"Word Category ID: {category.Id}");
+                Console.WriteLine($"Word Category Name: {category.Title}");
+                Console.WriteLine($"Word Category Description: {category.Description}");
+                Console.WriteLine("------------------------------");
+            }
+        }
+        else
+        {
+            Console.WriteLine(response.Message);
+        }
+    }
+}
