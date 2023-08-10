2f4f22b484ad37085c05306fe361a4874ed1beed
Iskandar <eiscocyber@gmail.com>
2023-08-10 14:43:23 +0500

    QuizControl Added question control updated

--- src/LexiLearn/Views/AdminView/QuestionControl.cs
+++ src/LexiLearn/Views/AdminView/QuestionControl.cs
@@ -1,183 +1,209 @@
-﻿using System;
-using LexiLearn.Domain.Enums;
+﻿using LexiLearn.Domain.Enums;
 using LexiLearn.Service.DTOs.Questions;
 using LexiLearn.Service.Services;
 
-namespace LexiLearn.Views.AdminView
+namespace LexiLearn.Views.AdminView;
+
+public class QuestionControl
 {
-    public class QuestionControl
+    private readonly QuestionService questionService;
+
+    public QuestionControl()
     {
-        private readonly QuestionService questionService;
+        questionService = new QuestionService();
+    }
 
-        public QuestionControl()
+    public async Task Start()
+    {
+        Console.WriteLine("Welcome to Question Management Console");
+
+        while (true)
         {
-            questionService = new QuestionService();
-        }
+            Console.WriteLine("\nOptions:");
+            Console.WriteLine("1. Create Question");
+            Console.WriteLine("2. Update Question");
+            Console.WriteLine("3. Delete Question");
+            Console.WriteLine("4. View All Questions");
+            Console.WriteLine("5. View Questions by Word");
+            Console.WriteLine("6. View Questions by Type");
+            Console.WriteLine("7. Exit");
 
-        public async Task Start()
-        {
-            Console.WriteLine("Welcome to Question Management Console");
+            Console.Write("Select an option: ");
+            var option = Console.ReadLine();
 
-            while (true)
+            switch (option)
             {
-                Console.WriteLine("\nOptions:");
-                Console.WriteLine("1. Create Question");
-                Console.WriteLine("2. Update Question");
-                Console.WriteLine("3. Delete Question");
-                Console.WriteLine("4. View All Questions");
-                Console.WriteLine("5. View Questions by Word");
-                Console.WriteLine("6. View Questions by Type");
-                Console.WriteLine("7. Exit");
-
-                Console.Write("Select an option: ");
-                var option = Console.ReadLine();
-
-                switch (option)
-                {
-                    case "1":
-                        await CreateQuestion();
-                        break;
-                    case "2":
-                        await UpdateQuestion();
-                        break;
-                    case "3":
-                        await DeleteQuestion();
-                        break;
-                    case "4":
-                        await ViewAllQuestions();
-                        break;
-                    case "5":
-                        await ViewQuestionsByWord();
-                        break;
-                    case "6":
-                        await ViewQuestionsByType();
-                        break;
-                    case "7":
-                        return;
-                    default:
-                        Console.WriteLine("Invalid option. Please select a valid option.");
-                        break;
-                }
+                case "1":
+                    await CreateQuestion();
+                    break;
+                case "2":
+                    await UpdateQuestion();
+                    break;
+                case "3":
+                    await DeleteQuestion();
+                    break;
+                case "4":
+                    await ViewAllQuestions();
+                    break;
+                case "5":
+                    await ViewQuestionsByWord();
+                    break;
+                case "6":
+                    await ViewQuestionsByType();
+                    break;
+                case "7":
+                    return;
+                default:
+                    Console.WriteLine("Invalid option. Please select a valid option.");
+                    break;
             }
-        }
-
-        private async Task CreateQuestion()
-        {
-            Console.Write("Enter question type (0 for EngToUzb, 1 for UzbToEng): ");
-            var questionType = (QuestionType)Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter word ID: ");
-            var wordId = Convert.ToInt64(Console.ReadLine());
-
-            var newQuestionDto = new QuestionCreationDto
-            {
-                Type = questionType,
-                WordId = wordId
-            };
-
-            var response = await questionService.CreateAsync(newQuestionDto);
-
-            if (response.StatusCode == 201)
-            {
-                Console.WriteLine("Question created successfully!");
-            }
-            else
-            {
-                Console.WriteLine($"Error creating question: {response.Message}");
-            }
-        }
-
-        private async Task UpdateQuestion()
-        {
-            Console.Write("Enter question ID to update: ");
-            var questionId = Convert.ToInt64(Console.ReadLine());
-
-            Console.Write("Enter question type (0 for EngToUzb, 1 for UzbToEng): ");
-            var questionType = (QuestionType)Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter word ID: ");
-            var wordId = Convert.ToInt64(Console.ReadLine());
-
-            var newQuestionDto = new QuestionUpdateDto
-            {
-                Id = questionId,
-                Type = questionType,
-                WordId = wordId
-            };
-
-            var response = await questionService.UpdateAsync(newQuestionDto);
-
-            if (response.StatusCode == 201)
-            {
-                Console.WriteLine("Question updated successfully!");
-            }
-            else
-            {
-                Console.WriteLine($"Error updating question: {response.Message}");
-            }
-        }
-
-        private async Task DeleteQuestion()
-        {
-            Console.Write("Enter question ID to delete: ");
-            var questionId = Convert.ToInt64(Console.ReadLine());
-
-            var response = await questionService.DeleteAsync(questionId);
-
-            if (response.StatusCode == 201)
-            {
-                Console.WriteLine("Question deleted successfully!");
-            }
-            else
-            {
-                Console.WriteLine($"Error deleting question: {response.Message}");
-            }
-        }
-
-        private async Task ViewAllQuestions()
-        {
-            var response = await questionService.GetAllAsync();
-
-            if (response.StatusCode == 200)
-            {
-                Console.WriteLine("All Questions:");
-                foreach (var question in response.Data)
-                {
-                    Console.WriteLine($"Question ID: {question.Id}, Type: {question.Type}, Word ID: {question.WordId}");
-                }
-            }
-            else
-            {
-                Console.WriteLine($"Error retrieving questions: {response.Message}");
-            }
-        }
-
-
-        private async Task ViewQuestionsByWord()
-        {
-            Console.Write("Enter word ID: ");
-            var wordId = Convert.ToInt64(Console.ReadLine());
-
-            var response = await questionService.GetQuestionsByWordAsync(wordId);
-
-            if (response.StatusCode == 200)
-            {
-                Console.WriteLine($"Questions for Word ID {wordId}:");
-                foreach (var question in response.Data)
-                {
-                    Console.WriteLine($"Question ID: {question.Id}, Type: {question.Type}");
-                }
-            }
-            else
-            {
-                Console.WriteLine($"Error retrieving questions: {response.Message}");
-            }
-        }
-
-
-        private async Task ViewQuestionsByType()
-        {
-            // ... (similar to CreateQuestion)
         }
     }
+
+    private async Task CreateQuestion()
+    {
+        Console.Write("Enter question type (0 for EngToUzb, 1 for UzbToEng): ");
+        var questionType = (QuestionType)Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter word ID: ");
+        var wordId = Convert.ToInt64(Console.ReadLine());
+
+        var newQuestionDto = new QuestionCreationDto
+        {
+            Type = questionType,
+            WordId = wordId
+        };
+
+        var response = await questionService.CreateAsync(newQuestionDto);
+
+        if (response.StatusCode == 201)
+        {
+            Console.WriteLine("Question created successfully!");
+        }
+        else
+        {
+            Console.WriteLine($"Error creating question: {response.Message}");
+        }
+    }
+
+    private async Task UpdateQuestion()
+    {
+        Console.Write("Enter question ID to update: ");
+        var questionId = Convert.ToInt64(Console.ReadLine());
+
+        Console.Write("Enter question type (0 for EngToUzb, 1 for UzbToEng): ");
+        var questionType = (QuestionType)Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter word ID: ");
+        var wordId = Convert.ToInt64(Console.ReadLine());
+
+        var newQuestionDto = new QuestionUpdateDto
+        {
+            Id = questionId,
+            Type = questionType,
+            WordId = wordId
+        };
+
+        var response = await questionService.UpdateAsync(newQuestionDto);
+
+        if (response.StatusCode == 201)
+        {
+            Console.WriteLine("Question updated successfully!");
+        }
+        else
+        {
+            Console.WriteLine($"Error updating question: {response.Message}");
+        }
+    }
+
+    private async Task DeleteQuestion()
+    {
+        Console.Write("Enter question ID to delete: ");
+        var questionId = Convert.ToInt64(Console.ReadLine());
+
+        var response = await questionService.DeleteAsync(questionId);
+
+        if (response.StatusCode == 201)
+        {
+            Console.WriteLine("Question deleted successfully!");
+        }
+        else
+        {
+            Console.WriteLine($"Error deleting question: {response.Message}");
+        }
+    }
+
+    private async Task ViewAllQuestions()
+    {
+        var response = await questionService.GetAllAsync();
+
+        if (response.StatusCode == 200)
+        {
+            Console.WriteLine("All Questions:");
+            foreach (var question in response.Data)
+            {
+                Console.WriteLine($"Question ID: {question.Id}, Type: {question.Type}, Word ID: {question.WordId}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Error retrieving questions: {response.Message}");
+        }
+    }
+
+
+    private async Task ViewQuestionsByWord()
+    {
+        Console.Write("Enter word ID: ");
+        var wordId = Convert.ToInt64(Console.ReadLine());
+
+        var response = await questionService.GetQuestionsByWordAsync(wordId);
+
+        if (response.StatusCode == 200)
+        {
+            Console.WriteLine($"Questions for Word ID {wordId}:");
+            foreach (var question in response.Data)
+            {
+                Console.WriteLine($"Question ID: {question.Id}, Type: {question.Type}, Word: {question.Word.Text}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Error retrieving questions: {response.Message}");
+        }
+    }
+
+
+    private async Task ViewQuestionsByType()
+    {
+        Console.WriteLine("Select a question type:");
+        Console.WriteLine("\n1. English To Uzbek");
+        Console.WriteLine("2. Uzbek To English");
+
+        Console.Write("Enter the number of the question type: ");
+        if (int.TryParse(Console.ReadLine(), out int selectedTypeNumber) && Enum.IsDefined(typeof(QuestionType), selectedTypeNumber))
+        {
+            QuestionType selectedType = (QuestionType)(selectedTypeNumber);
+
+            var response = await questionService.GetQuestionsByTypeAsync(selectedType);
+
+            if (response.StatusCode == 200)
+            {
+                Console.WriteLine($"Questions of Type: {selectedType}");
+                foreach (var question in response.Data)
+                {
+                    Console.WriteLine($"Question ID: {question.Id}, Word ID: {question.Word}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Error retrieving questions: {response.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid question type selection.");
+        }
+    }
+
 }
--- src/LexiLearn/Views/AdminView/QuizControl.cs
+++ src/LexiLearn/Views/AdminView/QuizControl.cs
@@ -1,12 +1,249 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LexiLearn.Domain.Enums;
+using LexiLearn.Service.DTOs.Quizzes;
+using LexiLearn.Service.Interfaces;
+using LexiLearn.Service.Services;
 
-namespace LexiLearn.Views.AdminView
+namespace LexiLearn.Views.AdminView;
+
+public class QuizControl
 {
-    internal class QuizControl
+    private readonly IQuizService quizService;
+
+    public QuizControl()
     {
+        quizService = new QuizService();
+    }
+
+    public async Task Start()
+    {
+        Console.WriteLine("Welcome to Quiz Management Console");
+
+        while (true)
+        {
+            Console.WriteLine("\nOptions:");
+            Console.WriteLine("1. Create Quiz");
+            Console.WriteLine("2. Update Quiz");
+            Console.WriteLine("3. Delete Quiz");
+            Console.WriteLine("4. View All Quizzes");
+            Console.WriteLine("5. View Quizzes by Category");
+            Console.WriteLine("6. View Quizzes by Level");
+            Console.WriteLine("7. Search Quizzes");
+            Console.WriteLine("8. Exit");
+
+            Console.Write("Select an option: ");
+            var option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    await CreateQuiz();
+                    break;
+                case "2":
+                    await UpdateQuiz();
+                    break;
+                case "3":
+                    await DeleteQuiz();
+                    break;
+                case "4":
+                    await ViewAllQuizzes();
+                    break;
+                case "5":
+                    await ViewQuizzesByCategory();
+                    break;
+                case "6":
+                    await ViewQuizzesByLevel();
+                    break;
+                case "7":
+                    await SearchQuizzes();
+                    break;
+                case "8":
+                    return;
+                default:
+                    Console.WriteLine("Invalid option. Please select a valid option.");
+                    break;
+            }
+        }
+    }
+
+    private async Task CreateQuiz()
+    {
+        var newQuizDto = new QuizCreationDto();
+
+        Console.Write("Enter title: ");
+        newQuizDto.Title = Console.ReadLine();
+
+        Console.Write("Enter description: ");
+        newQuizDto.Description = Console.ReadLine();
+
+        Console.Write("Enter category ID: ");
+        newQuizDto.CategoryId = Convert.ToInt64(Console.ReadLine());
+
+        Console.Write("Enter level (1 for Easy, 2 for Medium, 3 for Hard): ");
+        if (Enum.TryParse(Console.ReadLine(), out QuizLevel level))
+        {
+            newQuizDto.Level = level;
+
+            Console.Write("Enter deadline (in days): ");
+            newQuizDto.Deadline = Convert.ToInt32(Console.ReadLine());
+
+            var response = await quizService.CreateAsync(newQuizDto);
+
+            if (response.StatusCode == 200)
+            {
+                Console.WriteLine("Quiz created successfully!");
+            }
+            else
+            {
+                Console.WriteLine($"Error creating quiz: {response.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid level selection.");
+        }
+    }
+
+    private async Task UpdateQuiz()
+    {
+        var newQuizDto = new QuizUpdateDto();
+
+        Console.Write("Enter ID: ");
+        newQuizDto.Id = Convert.ToInt64(Console.ReadLine());
+
+        Console.Write("Enter title: ");
+        newQuizDto.Title = Console.ReadLine();
+
+        Console.Write("Enter description: ");
+        newQuizDto.Description = Console.ReadLine();
+
+        Console.Write("Enter category ID: ");
+        newQuizDto.CategoryId = Convert.ToInt64(Console.ReadLine());
+
+        Console.Write("Enter level (1 for Easy, 2 for Medium, 3 for Hard): ");
+        if (Enum.TryParse(Console.ReadLine(), out QuizLevel level))
+        {
+            newQuizDto.Level = level;
+
+            Console.Write("Enter deadline (in days): ");
+            newQuizDto.Deadline = Convert.ToInt32(Console.ReadLine());
+
+            var response = await quizService.UpdateAsync(newQuizDto);
+
+            if (response.StatusCode == 200)
+            {
+                Console.WriteLine("Quiz updated successfully!");
+            }
+            else
+            {
+                Console.WriteLine($"Error updating quiz: {response.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid level selection.");
+        }
+    }
+
+    private async Task DeleteQuiz()
+    {
+        Console.Write("Enter quiz ID: ");
+        var quizId = Convert.ToInt64(Console.ReadLine());
+
+        var response = await quizService.DeleteAsync(quizId);
+
+        if (response.StatusCode == 200)
+        {
+            Console.WriteLine("Quiz updated successfully!");
+        }
+        else
+        {
+            Console.WriteLine($"Error updating quiz: {response.Message}");
+        }
+    }
+
+    private async Task ViewAllQuizzes()
+    {
+        var response = await quizService.GetAllAsync();
+
+        if (response.StatusCode == 200)
+        {
+            Console.WriteLine("All Quizzes:");
+            foreach (var quiz in response.Data)
+            {
+                Console.WriteLine($"Quiz ID: {quiz.Id}, Title: {quiz.Title}, Description: {quiz.Description}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Error retrieving quizzes: {response.Message}");
+        }
+    }
+
+    private async Task ViewQuizzesByCategory()
+    {
+        Console.Write("Enter category ID: ");
+        var categoryId = Convert.ToInt64(Console.ReadLine());
+
+        var response = await quizService.GetQuizzesByCategoryAsync(categoryId);
+
+        if (response.StatusCode == 200)
+        {
+            Console.WriteLine($"Quizzes in Category {categoryId}:");
+            foreach (var quiz in response.Data)
+            {
+                Console.WriteLine($"Quiz ID: {quiz.Id}, Title: {quiz.Title}, Description: {quiz.Description}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Error retrieving quizzes: {response.Message}");
+        }
+    }
+
+    private async Task ViewQuizzesByLevel()
+    {
+        Console.Write("Enter level (1 for Easy, 2 for Medium, 3 for Hard, 4 for Impossible): ");
+        if (Enum.TryParse(Console.ReadLine(), out QuizLevel level))
+        {
+            var response = await quizService.GetQuizzesByLevelAsync(level);
+
+            if (response.StatusCode == 200)
+            {
+                Console.WriteLine($"Quizzes in Level {level}:");
+                foreach (var quiz in response.Data)
+                {
+                    Console.WriteLine($"Quiz ID: {quiz.Id}, Title: {quiz.Title}, Description: {quiz.Description}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Error retrieving quizzes: {response.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid level selection.");
+        }
+    }
+
+    private async Task SearchQuizzes()
+    {
+        Console.Write("Enter search term: ");
+        var searchTerm = Console.ReadLine();
+
+        var response = await quizService.SearchQuizzesAsync(searchTerm);
+
+        if (response.StatusCode == 200)
+        {
+            Console.WriteLine($"Quizzes matching search term '{searchTerm}':");
+            foreach (var quiz in response.Data)
+            {
+                Console.WriteLine($"Quiz ID: {quiz.Id}, Title: {quiz.Title}, Description: {quiz.Description}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Error retrieving quizzes: {response.Message}");
+        }
     }
 }
